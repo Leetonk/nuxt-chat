768b8e49f940bea9fc3899afb0c2ff517d93848a
Anton Tarasyuk <tarasyuk@1221systems.ru>
2026-02-07 02:15:42 +0300

    test

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -3,7 +3,7 @@ import { createOpenAIModel, generateChatResponse } from '#server/services/ai-ser
 export default defineEventHandler(async (event: EventHandlerResponse) => {
   const body = await readBody(event)
   const { messages } = body
-
+  console.log(useRuntimeConfig())
   const id = messages.length.toString()
   const openaiApiKey = useRuntimeConfig().openaiApiKey
 
